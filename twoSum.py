cad0ce3c7a6626837eec87ec89e68a285cdbc705
zdickerson <zachary-dickerson@uiowa.edu>
2022-11-08 23:05:05 -0600

    Update to twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -7,7 +7,12 @@ target2 = 12
 arr3 = [1,3,7,9,2]
 target3 = 11
 
+arr4 = [2,7,11,15]
+target4 = 9
 
+''' 
+Optimal Solution
+'''
 def two_sum(our_array, target):
 
     values = dict()
@@ -23,11 +28,43 @@ def two_sum(our_array, target):
 
     return []
 
-list1 = two_sum(arr1, target1)
-print(list1)
+'''
+    Brute  Force
+    Time   O(n^2)
+    Space  O(1)
+'''
+def two_sum2(nums, target):
 
-list2 = two_sum(arr2, target2)
-print(list2)
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
 
-list3 = two_sum(arr3, target3)
-print(list3)
+
+def two_sum3(nums, target):
+
+    values = {} # constant time access
+
+    for i, element in enumerate(nums):
+
+        if element in values:
+
+            return [values[element], i]
+
+        values[target - element] = i
+    
+    return []
+
+
+# list1 = two_sum(arr1, target1)
+# print(list1)
+
+# list2 = two_sum(arr2, target2)
+# print(list2)
+
+# list3 = two_sum(arr3, target3)
+# print(list3)
+
+list4 = two_sum2(arr4, target4)
+print(list4)
